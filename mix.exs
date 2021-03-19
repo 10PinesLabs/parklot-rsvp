88450dc03db4862fa267d3a4a432ec57bfe0e49b
Charlyzzz <erwincdl@gmail.com>
2021-03-19 20:02:06 -0300

    updates elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ParklotRsvp.Mixfile do
     [
       app: :parklot_rsvp,
       version: "0.0.1",
-      elixir: "~> 1.4",
+      elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
